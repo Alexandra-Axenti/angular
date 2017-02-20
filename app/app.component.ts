6ea80716ef0d9597cbd55fecdf9b608b4895aba5
Alexandra-Axenti <axentialexandra@yahoo.com>
2017-02-20 17:42:12 +0800

    added and separated functional tasks and features

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,32 +1,37 @@
 import { Component } from '@angular/core';
+import { Task } from './task.model';
 
 @Component({
   selector: 'my-app',
   template: `
   <div class="container">
     <h1>My First Angular 2 App</h1>
-    <h3 (click)="doStuff(currentTask)" *ngFor="let currentTask of tasks">{{ currentTask.description }}</h3>
+    <pies></pies>
+    <task-list
+      [childTaskList]="masterTaskList"
+      (clickSender)="showDetails($event)"
+     ></task-list>
+    <edit-task
+      [childSelectedTask]="selectedTask"
+      (doneClickedSender)="finishedEditing()"
+      ></edit-task>
   </div>
   `
 })
 
 export class AppComponent {
-  public tasks: Task[] = [
-      new Task("Create To-Do List app.", 0),
-      new Task("Learn Kung Fu.", 1),
-      new Task("Rewatch all the Lord of the Rings movies.", 2),
-      new Task("Do the laundry.", 3)
-  ];
-  doStuff(clickedTask: Task) {
-    if(clickedTask.done === true) {
-      alert("This task is done!");
-    } else {
-      alert("This task is not completed yet! Get to work!");
-    }
-  }
-}
+      public masterTaskList: Task[] = [
+          new Task("Create To-Do List app.", 0),
+          new Task("Learn Kung Fu.", 1),
+          new Task("Rewatch all the Lord of the Rings movies.", 2),
+          new Task("Do the laundry.", 3)
+      ];
 
-export class Task {
-  public done: boolean = false;
-  constructor (public description: string, public id: number) {  }
+  selectedTask: Task = null;
+  showDetails(clickedTask: Task) {
+    this.selectedTask = clickedTask;
+  }
+  finishedEditing() {
+    this.selectedTask = null;
+  }
 }
--- app/app.module.ts
+++ app/app.module.ts
@@ -1,10 +1,23 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule }   from '@angular/forms';
 import { AppComponent }   from './app.component';
+import { PiesListComponent } from './pies-list.component';
+import { TaskListComponent } from './task-list.component';
+import { EditTaskComponent } from './edit-task.component';
+
 
 @NgModule({
-  imports: [BrowserModule],
-  declarations: [ AppComponent ],
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  declarations: [
+    PiesListComponent,
+    TaskListComponent,
+    AppComponent,
+    EditTaskComponent,
+  ],
   bootstrap:    [ AppComponent ]
 })
 
